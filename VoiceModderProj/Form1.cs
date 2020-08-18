13dbc4d34c46724ace2019675a57a82ed6295aa7
TheELewis <enlewis@ualberta.ca>
2020-08-18 17:41:00 -0600

    Simple recording functionality done.

--- VoiceModderProj/Form1.cs
+++ VoiceModderProj/Form1.cs
@@ -17,34 +17,78 @@ namespace VoiceModderProj
 {
     public partial class Form1 : Form
     {
+        private int RATE = 44100; //sound card sample rate, may need tweaking
+        //private int BUFFSIZE = (int)(Math.Pow(2, 11)); //Must be a multiple of 2
+
+        private BufferedWaveProvider bwp;
+        private WaveIn recorder;
+        private WaveOut player;
+        bool recordToggle = false;
+
         public Form1()
         {
             InitializeComponent();
-            //StartListeningToMicrophone();
             populatecombobox();
         }
+
+        //Make dropdown menu
         public void populatecombobox() {
             int waveInDevices = WaveIn.DeviceCount;
             for (int waveInDevice = 0; waveInDevice < waveInDevices; waveInDevice++)
             {
                 WaveInCapabilities deviceInfo = WaveIn.GetCapabilities(waveInDevice);
-                //make dropdown menu
-                //Console.WriteLine("Device {0}: {1}, {2} channels", waveInDevice, deviceInfo.ProductName, deviceInfo.Channels);
                 comboBox1.Items.Add(deviceInfo.ProductName);
             }
+        }
+
+        //When the selected audio device has audio ready,
+        // add this audio in samples to the buffer.
+        void RecorderDataAvailable(object sender, WaveInEventArgs args) 
+        {
+            bwp.AddSamples(args.Buffer, 0, args.BytesRecorded);
         }
 
         public void StartListeningToMicrophone()
         {
             //throw new NotImplementedException();
-            WaveIn wi = new WaveIn();
-            wi.DeviceNumber = GetInputDevice();
-            wi.WaveFormat = new NAudio.
+            int audioDeviceNumber = GetInputDevice();
+            recorder = new WaveIn()
+            {
+                DeviceNumber = audioDeviceNumber,
+                WaveFormat = new WaveFormat(RATE, 1)
+            };
+            recorder.DataAvailable += new EventHandler<WaveInEventArgs>(RecorderDataAvailable);
+
+            bwp = new BufferedWaveProvider(recorder.WaveFormat);
+
+            //recorder = new WaveIn()
+            //{
+            //    DeviceNumber = audioDeviceNumber,
+            //    WaveFormat = new NAudio.Wave.WaveFormat(RATE, 1),
+            //    BufferMilliseconds = (int)((double)BUFFSIZE / (double)RATE * 1000.0)
+            //};
+            //recorder.DataAvailable += new EventHandler<WaveInEventArgs>(RecorderDataAvailable);
+
+            //bwp = new BufferedWaveProvider(recorder.WaveFormat)
+            //{
+            //    BufferLength = BUFFSIZE * 2,
+            //    DiscardOnBufferOverflow = true
+            //};
+
+            player = new WaveOut();
+            player.Init(bwp);
+            player.Play();
+            recorder.StartRecording();
+        }
+
+        public void StopListeningToMicrophone() 
+        {
+            recorder.StopRecording();
+            player.Stop();
         }
 
         private int GetInputDevice()
         {
-            //throw new NotImplementedException();
             //get current dropdown value
             return comboBox1.SelectedIndex;
         }
@@ -56,18 +100,23 @@ namespace VoiceModderProj
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //wi.DeviceNumber = GetInputDevice();
+            //recorder.DeviceNumber = GetInputDevice();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Button1_Click(object sender, EventArgs e)
         {
-            int waveInDevices = WaveIn.DeviceCount;
-            for (int waveInDevice = 0; waveInDevice < waveInDevices; waveInDevice++)
+            Control ctrl = ((Control)sender);
+            if (!recordToggle)
             {
-                WaveInCapabilities deviceInfo = WaveIn.GetCapabilities(waveInDevice);
-                //make dropdown menu
-                //Console.WriteLine("Device {0}: {1}, {2} channels", waveInDevice, deviceInfo.ProductName, deviceInfo.Channels);
-                comboBox1.Items.Add(deviceInfo.ProductName);
+                StartListeningToMicrophone();
+                recordToggle = true;
+                ctrl.BackColor = Color.Red;
+            }
+            else if (recordToggle)
+            {
+                StopListeningToMicrophone();
+                recordToggle = false;
+                ctrl.BackColor = Color.Gray;
             }
         }
     }
